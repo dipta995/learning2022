6f768a716fa5ee3473b7e8d526cb5bdc54f2b247
dipta995 <dipta995@gmail.com>
2022-03-07 23:50:55 +0600

    sql add

--- course_sell.sql
+++ course_sell.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2022 at 07:00 PM
+-- Generation Time: Mar 07, 2022 at 06:50 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -20,6 +20,55 @@ SET time_zone = "+00:00";
 --
 -- Database: `course_sell`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(191) NOT NULL,
+  `is_active` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_name`, `is_active`) VALUES
+(2, 'cat one ', 0),
+(3, 'cat two', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `course_title` varchar(191) NOT NULL,
+  `banner` varchar(191) NOT NULL,
+  `short_description` text NOT NULL,
+  `description` longtext NOT NULL,
+  `price` varchar(191) NOT NULL,
+  `discount` varchar(100) NOT NULL,
+  `hours` varchar(100) NOT NULL,
+  `total_hit` int(11) NOT NULL,
+  `ratting` int(11) NOT NULL,
+  `is_active` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`course_id`, `category_id`, `course_title`, `banner`, `short_description`, `description`, `price`, `discount`, `hours`, `total_hit`, `ratting`, `is_active`, `created_at`) VALUES
+(2, 2, 'course ione ', 'image/b480f755fb.png', ' course ione  ', 'course ione  ', '222', '22', '23', 0, 0, 0, '2022-03-07 17:18:11');
 
 -- --------------------------------------------------------
 
@@ -44,11 +93,24 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `password`, `role`, `is_active`, `created_at`) VALUES
-(1, 'Dipta', 'Dey', 'dipta995@gmail.com', '01632315608', '12345678', 'user', 0, '2022-02-23 17:46:50');
+(1, 'Dipta', 'Dey', 'dipta995@gmail.com', '01632315608', '12345678', 'admin', 0, '2022-03-01 16:53:03');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `categor table` (`category_id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +123,32 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `categor table` FOREIGN KEY (`category_id`) REFERENCES `categories` (`cat_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
